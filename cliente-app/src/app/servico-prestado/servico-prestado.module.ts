f5a83cfafb65d7140af1dc69ce42ad3af28cfd8a
douglas <voc@example.com>
2021-01-17 14:35:10 -0300

    Criação do servico prestado form e list

--- cliente-app/src/app/servico-prestado/servico-prestado.module.ts
+++ cliente-app/src/app/servico-prestado/servico-prestado.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 import { ServicoPrestadoRoutingModule } from './servico-prestado-routing.module';
 import { ServicoPrestadoFormComponent } from './servico-prestado-form/servico-prestado-form.component';
+import { ServicoPrestadoListComponent } from './servico-prestado-list/servico-prestado-list.component';
 
 
 @NgModule({
-  declarations: [ServicoPrestadoFormComponent],
+  declarations: [ServicoPrestadoFormComponent, ServicoPrestadoListComponent],
   imports: [
     CommonModule,
-    ServicoPrestadoRoutingModule
+    ServicoPrestadoRoutingModule,
+    FormsModule
+  ],
+  exports: [
+    ServicoPrestadoFormComponent,
+    ServicoPrestadoListComponent
   ]
+
 })
 export class ServicoPrestadoModule { }
