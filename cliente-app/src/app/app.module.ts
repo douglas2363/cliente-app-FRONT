f1b6780cbb5f5fad235e390f018914a0cb61695f
douglas <voc@example.com>
2021-01-29 09:56:06 -0300

    Criação do layout para roteamento para pagina de login

--- cliente-app/src/app/app.module.ts
+++ cliente-app/src/app/app.module.ts
@@ -1,21 +1,29 @@
-import { ServicoPrestadoService } from './servico-prestado.service';
-import { ClientesModule } from './clientes/clientes.module';
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-import {TemplateModule} from './template/template.module';
-import { HomeComponent } from './home/home.component';
-import { ClientesService } from './clientes.service';
 import {ServicoPrestadoModule} from './servico-prestado/servico-prestado.module';
+import { AppRoutingModule } from './app-routing.module';
+import {TemplateModule} from './template/template.module';
+import { ClientesModule } from './clientes/clientes.module';
+import {FormsModule} from '@angular/forms'
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+import { ClientesService } from './clientes.service';
+import { ServicoPrestadoService } from './servico-prestado.service';
+import { LayoutComponent } from './layout/layout.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    LayoutComponent
     
   ],
   imports: [
@@ -24,7 +32,8 @@ import {ServicoPrestadoModule} from './servico-prestado/servico-prestado.module'
     AppRoutingModule,
     TemplateModule,
     ClientesModule,
-    ServicoPrestadoModule
+    ServicoPrestadoModule,
+    FormsModule
 
   ],
   providers: [
