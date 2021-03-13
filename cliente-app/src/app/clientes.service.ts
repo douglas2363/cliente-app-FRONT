ebb72b8bda8b501aab69c4ed4ac0ed5654d54304
Douglas Lima <dnlima@latam.stefanini.com.br>
2021-03-13 16:46:48 -0300

    Guardando tokem da requisição no localStorage

--- cliente-app/src/app/clientes.service.ts
+++ cliente-app/src/app/clientes.service.ts
@@ -18,13 +18,23 @@ export class ClientesService {
   ) { } 
 
   salvar(cliente : Cliente) : Observable<Cliente>{
-      return  this.http.post<Cliente>(`${this.apiURL}`, cliente);
+    const tokenString = localStorage.getItem('access_token')
+    const token = JSON.parse(tokenString)
+    const headers = {
+        'Authorization' : 'Bearer' + token.access_token
+    }
+    return  this.http.post<Cliente>(`${this.apiURL}`, cliente, {headers});
   }
   atualizar(cliente: Cliente): Observable<any>{
     return this.http.put<Cliente>(`${this.apiURL}/${cliente.id}`, cliente);
   }
   getClientes(): Observable<Cliente[]>{
-    return this.http.get<Cliente[]>(this.apiURL);
+    const tokenString = localStorage.getItem('access_token')
+    const token = JSON.parse(tokenString)
+    const headers = {
+        'Authorization' : 'Bearer' + token.access_token
+    }
+    return this.http.get<Cliente[]>(this.apiURL, {headers});
   }
   getClientesById(id: number): Observable<Cliente>{
     return this.http.get<any>(`${this.apiURL}/${id}`);
